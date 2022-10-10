e648b4306a393bfeff45f8b47f30d0b463c15b19
Adrián Borrageiros Mourelos <aborrageirosm@fpcoruna.afundacion.org>
2022-10-10 13:17:31 +0200

    PYTHON 4

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,3 +1,7 @@
 from factorial import func_factorial as factorial
-print("Introduce un número: ")
+from factorial2 import func_factorial2 as factorial2
+print("Introduce un número:  (func_factorial)")
 print( factorial(int(input())) )
+print()
+print("Introduce un número:  (func_factorial2)")
+print( factorial2(int(input())) )
