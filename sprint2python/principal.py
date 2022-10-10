9edbca098860571fdfb6ccc686d7a71ff267c1af
Adrián Borrageiros Mourelos <aborrageirosm@fpcoruna.afundacion.org>
2022-10-10 13:26:17 +0200

    PYTHON 6

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,6 +1,6 @@
 from factorial import func_factorial as factorial
 from factorial2 import func_factorial2 as factorial2
-
+import time
 
 op = ''
 while (op!='a' and op!='b'):
@@ -14,8 +14,15 @@ print()
 
 if (op=='a'):
     print("Introduce un número:  ")
+    start_time = time.time()
     print( factorial(int(input())) )
+    end_time = time.time()
+    elapsed_time = end_time - start_time
 else:
     print("Introduce un número:  ")
+    start_time = time.time()
     print( factorial2(int(input())) )
+    end_time = time.time()
+    elapsed_time = end_time - start_time
 
+print("El tiempo de ejecución ha sido: ", elapsed_time, " s")
