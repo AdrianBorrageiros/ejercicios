5566d20c3973f5ed1eb45cd647dc6e58ebd25f36
Adrián Borrageiros Mourelos <aborrageirosm@fpcoruna.afundacion.org>
2022-10-19 14:47:44 +0200

    Sesiones ejercicio 3

--- sprint3sesiones/www/mysite/login.php
+++ sprint3sesiones/www/mysite/login.php
@@ -2,8 +2,9 @@
     $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
     $email_posted = $_POST['f_email'];
     $password_posted = $_POST['f_password'];
-    $query = "SELECT id, contraseña FROM tUsuarios WHERE email = '".$email_posted."'";
+    $query = "SELECT id, contrasena FROM tUsuarios WHERE email = '".$email_posted."'";
     $result = mysqli_query($db, $query) or die('Query error');
+    console.log($result);
     if (mysqli_num_rows($result) > 0) {
         $only_row = mysqli_fetch_array($result);
         if ($only_row[1] == $password_posted) {
@@ -14,6 +15,6 @@
             echo '<p>Contraseña incorrecta</p>';
         }
         } else {
-            echo '<p>Usuario no encontrado con ese email</p>';
+            echo '<p>Email no encontrado</p>';
     }
 ?>
