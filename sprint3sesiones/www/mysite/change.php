ed26d9b8fbbff5521bc2ae5d58cfa0c9da1c7ccd
Adrián Borrageiros Mourelos <aborrageirosm@fpcoruna.afundacion.org>
2022-10-20 14:19:52 +0200

    EJERCICIO 7 - CAMBIO DE CONTRASEÑA

---
+++ sprint3sesiones/www/mysite/change.php
@@ -0,0 +1,18 @@
+<?php
+    $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+    session_start();
+    $query = "SELECT contrasena FROM tUsuarios WHERE id = '".$_SESSION['user_id']."'";
+    $result = mysqli_query($db, $query) or die('Query error');
+    
+    $password_before = $_POST['bf_password'];
+    $password_after = $_POST['af_password'];
+
+    $only_row = mysqli_fetch_array($result);
+    if ($only_row[0] == $password_before) {
+        $query = "UPDATE tUsuarios SET contrasena='".$password_after."' WHERE id='".$_SESSION['user_id']."'";
+        $result = mysqli_query($db, $query) or die('Query error');
+        header('Location: main.php');
+    }else {
+        echo 'Contraseña antigua incorrecta';
+    }
+?>
---
+++ sprint3sesiones/www/mysite/changePWD.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <?php
+            $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+            session_start();
+            $query = "SELECT contrasena FROM tUsuarios WHERE id = '".$_SESSION['user_id']."'";
+            $result = mysqli_query($db, $query) or die('Query error');
+
+            if (mysqli_num_rows($result) > 0) {
+                echo '<h1>CAMBIO DE CONTRASEÑA</h1>';
+                echo '<form id="formulario" action="change.php" method="post">';
+                echo '<input id="claveAntes" name="bf_password" type="password" placeholder="Contraseña antigua"><br>';
+                echo '<input id="claveAhora" name="af_password" type="password" placeholder="Contraseña nueva"><br>';
+                echo '<input type="submit" value="Cambiar contraseña">';
+                echo '</form>';
+            }else {
+                echo '<p>No has iniciado sesión</p>';
+                echo '<a href="/login.html">INICIAR SESIÓN</a>';
+            }
+        ?>
+    </body>
+</html>
